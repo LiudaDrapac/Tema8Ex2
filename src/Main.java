bcd6252d59e770d1d9ffedc79a398d34b1ccd97f
Ludmila <ludmila.drapac@cnas.gov.md>
2023-10-15 00:47:40 -0800

    2

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,10 @@ import parallelogram.Parallelogram;
 public class Main {
     public static void main(String[] args) {
         Parallelogram parallelogram = new Parallelogram();
-        parallelogram.calculatePerimeter(15, 10);
+        parallelogram.calculatePerimeter(23, 15);
+        parallelogram.calculatePerimeter(12);
         parallelogram.calculateArea(11, 8);
+        parallelogram.calculateArea(9);
 
 
 }}
--- src/parallelogram/Parallelogram.java
+++ src/parallelogram/Parallelogram.java
@@ -9,13 +9,15 @@ public class Parallelogram {
     }
 
     public int calculatePerimeter(int sideLength) {
-      return sideLength;
+        System.out.println("sideLength Perimeter = " + sideLength);
+    return sideLength;
     }
     public double calculateArea(int length, int height) {
         System.out.println("Aria = " + length * height);
         return length * height;
     }
     public int calculateArea(int sideLength) {
+        System.out.println("sideLength Aria = " + sideLength);
         return sideLength;
     }
 
